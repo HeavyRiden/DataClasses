246b95044cbafa6a5f28d69a9349ca426efcfbf3
Kirill Novichkov <sufirhavat@gmail.com>
2025-01-30 19:51:55 +0700

    Added class instance

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
-
 data class Post(
     val id: Int, // Идентификатор записи
     val ownerId: Int, // Идентификатор владельца стены, на которой размещена запись
@@ -7,14 +6,13 @@ data class Post(
     val text: String, // Текст записи
     val markedAsAds: Boolean, // Информация о том, содержит ли запись отметку «реклама»
     val isFavorite: Boolean, // true, если объект добавлен в закладки у текущего пользователя
-    val postponedId: Int, // Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере
     val signerId: Int, // Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
     val canPin: Boolean, // Информация о том, может ли текущий пользователь закрепить запись
     val comment: Comments // Информация о комментариях к записи, объект с полями
 
 )
 
-class Comments (
+class Comments(
     private var count: Int, // количество комментариев
     private var canPost: Boolean, // информация о том, может ли текущий пользователь комментировать запись
     private var groupsCanPost: Boolean, // информация о том, могут ли сообщества комментировать запись;
@@ -25,11 +23,31 @@ class Comments (
 object WallService {
     private var posts = emptyArray<Post>()
 
-    fun add(post: Post): Post {
+    fun add(post: Post) {
         posts += post
-        return posts.last()
     }
 }
 
 fun main() {
+
+    var firstPost = Post(
+        id = 0,
+        ownerId = 0,
+        fromId = 0,
+        createdBy = 0,
+        text = "Первый пост",
+        markedAsAds = false,
+        isFavorite = true,
+        signerId = 0,
+        canPin = true,
+        comment = Comments(
+            count = 0,
+            canPost = true,
+            groupsCanPost = true,
+            canClose = true,
+            canOpen = true
+        )
+    )
+
+    WallService.add(firstPost)
 }
